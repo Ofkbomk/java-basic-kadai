927af56ef87299e498b03145942ecaec4aec31d6
Ofkbomk <kmeiofkbo@gmail.com>
2025-01-03 23:59:12 +0900

    kadai_026を追加

---
+++ kadai_026/JyankenExec_Chapter26.java
@@ -0,0 +1,20 @@
+package kadai_026;
+
+public class JyankenExec_Chapter26 {
+
+	public static void main(String[] args) {
+		// じゃんけんクラスのインスタンスを作成する
+				Jyanken_Chapter26 jyanken = new Jyanken_Chapter26();
+
+				// 自分のじゃんけんの手を入力する
+				String myChoice = jyanken.getMyChoice();
+
+				// 対戦相手のじゃんけんの手が乱数で選ばれる
+				String random = jyanken.getRandom();
+
+				// じゃんけんを行う
+				jyanken.playGame(myChoice, random);
+
+	}
+
+}
